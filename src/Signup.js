b97d841dfd4091aba58859cbbfe349beb533b719
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-05-08 03:05:19 -0600

    Testing for mobile devices

--- src/Signup.js
+++ src/Signup.js
@@ -8,9 +8,13 @@ class Signup extends Component {
         return (
             <>
                 <div>
+                    {/*<iframe*/}
+                    {/*    src="https://docs.google.com/forms/d/e/1FAIpQLScuDmHgJlah50uTFYXwT7otYFRBiZf6E0ujy-FFADrBsPWy2g/viewform?embedded=true"*/}
+                    {/*    width="640" height="1448" frameBorder="0" marginHeight="0" marginWidth="0">Loading...*/}
+                    {/*</iframe>*/}
                     <iframe
                         src="https://docs.google.com/forms/d/e/1FAIpQLScuDmHgJlah50uTFYXwT7otYFRBiZf6E0ujy-FFADrBsPWy2g/viewform?embedded=true"
-                        width="640" height="1448" frameBorder="0" marginHeight="0" marginWidth="0">Loading...
+                        width="450" height="1448" frameBorder="0" marginHeight="0" marginWidth="0">Loading...
                     </iframe>
                 </div>
             </>
